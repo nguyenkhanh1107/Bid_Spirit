01e7a1f9c2282fbad83a420425fad1d3a810c380
khoa <tkhoa1122@gmail.com>
2024-10-05 19:03:47 +0700

    detail pages

--- app/Http/Controllers/DetailsController.php
+++ app/Http/Controllers/DetailsController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Item;
 use Illuminate\Http\Request;
 
 class DetailsController extends Controller
 {
-    public function index() {
-        return view('pages.detailspage');
+    public function index($id)
+    {
+        // Lấy sản phẩm theo ID
+        $item = Item::with('auction')->findOrFail($id);
+
+        // Trả dữ liệu về trang chi tiết sản phẩm
+        return view('pages.detailspage', compact('item'));
     }
 }
